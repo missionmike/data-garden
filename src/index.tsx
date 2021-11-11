669ac055d6d0720977cd3d10e7f1cf0e44422092
Michael Dinerstein <md@missionmike.dev>
2021-11-10 23:20:59 -0800

    Revert to BrowserRouter
    Test using HashRouter didn't provide results I was looking for; reverting to BrowserRouter and including static _redirects file for Netlify

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import { HashRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 
 import HashMap from "./pages/HashMap.mdx";
 import Home from "./pages/Home.mdx";
@@ -6,11 +6,11 @@ import React from "react";
 import { render } from "react-dom";
 
 render(
-  <HashRouter>
+  <BrowserRouter>
     <React.Fragment>
       <Route exact path="/" component={Home} />
       <Route exact path="/hashmap/" component={HashMap} />
     </React.Fragment>
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
